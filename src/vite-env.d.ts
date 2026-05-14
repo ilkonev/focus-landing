83f13149516b6a977fabc943e566543624b2c03c
Ekaterina <kibanova16@mail.ru>
2026-05-14 16:47:35 +0300

    remove GA plug for TS

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,17 +1,8 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_GA_MEASUREMENT_ID?: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
-
 declare global {
   interface Window {
     dataLayer?: unknown[];
-    gtag?: (...args: unknown[]) => void;
     ym?: (id: number, action: string, ...args: unknown[]) => void;
   }
 }
